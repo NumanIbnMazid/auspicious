7729f6624fac7d58f284a94ca5a5fe62992f9704
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-05-30 15:49:22 +0600

    settings file

--- auspicious/settings/development.py
+++ auspicious/settings/development.py
@@ -1,5 +1,14 @@
 from auspicious.settings.common import *
 from decouple import config, Csv
+#........env file add.......
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+import environ
+env = environ.Env()
+env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
+
+
+#............end.......
 
 
 SECRET_KEY = config('SECRET_KEY')
@@ -22,12 +31,17 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 # Database
 
 # ======= SQLITE3 =======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env.str('DATABASE_URL', default=''):
+    DATABASES = {
+        'default': env.db(),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ======= MYSQL =======
 # DATABASES = {
