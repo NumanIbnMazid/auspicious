5d4f57acf8d368281c9a91b4fd01cbd7401272b0
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-06-01 15:39:06 +0600

    all static page add

--- auspicious/urls.py
+++ auspicious/urls.py
@@ -5,10 +5,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # Import Views
-from .views import HomeView
+from .views import HomeView,AboutView, CivilProjectView, TelecomProjectView, \
+    CivilServicesView, TelecomServicesView, CareerView, ClientView, NewsView,\
+    ContactView, NewsDetailsView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("civil-project/", CivilProjectView.as_view(), name="civil_project"),
+    path("telecom-project/", TelecomProjectView.as_view(), name="telecom_project"),
+    path("civil-service/", CivilServicesView.as_view(), name="civil_service"),
+    path("telecom-service/", TelecomServicesView.as_view(), name="telecom_service"),
+    path("career/", CareerView.as_view(), name="career"),
+    path("sister & client/", ClientView.as_view(), name="client"),
+    path("news/", NewsView.as_view(), name="news"),
+    path("contact/", ContactView.as_view(), name="contact"),
+    path("news-details/", NewsDetailsView.as_view(), name="news_details"),
     path("admin/", admin.site.urls),
     path("account/", include("accounts.urls")),
     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
--- auspicious/views.py
+++ auspicious/views.py
@@ -2,3 +2,33 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+class AboutView(TemplateView):
+    template_name = 'page/about.html'
+
+class CivilProjectView(TemplateView):
+    template_name = 'page/civil-project.html'
+
+class TelecomProjectView(TemplateView):
+    template_name = 'page/telecom-project.html'
+
+class CivilServicesView(TemplateView):
+    template_name = 'page/civil-service.html'
+
+class TelecomServicesView(TemplateView):
+    template_name = 'page/telecom-service.html'
+
+class CareerView(TemplateView):
+    template_name = 'page/career.html'
+
+class ClientView(TemplateView):
+    template_name = 'page/client.html'
+
+class NewsView(TemplateView):
+    template_name = 'page/news.html'
+
+class ContactView(TemplateView):
+    template_name = 'page/contact.html'
+
+class NewsDetailsView(TemplateView):
+    template_name = 'page/news-details.html'
