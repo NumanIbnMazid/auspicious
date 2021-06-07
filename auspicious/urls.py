6abfb3d974142c58301b29a945177ab6b27e0bb1
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-06-07 11:56:19 +0600

    Updated

--- auspicious/urls.py
+++ auspicious/urls.py
@@ -7,7 +7,7 @@ from . import views
 # Import Views
 from .views import  AboutView, \
     CivilServicesView, TelecomServicesView, ClientView, NewsView,\
-    ContactView, NewsDetailsView, JobApplyCreateView
+    NewsDetailsView, JobApplyCreateView
 
 urlpatterns = [
     # path("", HomeView.as_view(), name="home"),
@@ -24,7 +24,8 @@ urlpatterns = [
     path("job-details/<slug>/", views.job_details, name="job_details"),
     path("sister & client/", ClientView.as_view(), name="client"),
     path("news/", NewsView.as_view(), name="news"),
-    path("contact/", ContactView.as_view(), name="contact"),
+    # path("contact/", ContactView.as_view(), name="contact"),
+    path("contact/", views.contact, name="contact"),
     path("news-details/", NewsDetailsView.as_view(), name="news_details"),
     path("admin/", admin.site.urls),
     path("account/", include("accounts.urls")),
